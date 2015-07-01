69087e0a80b998534da7480c86b6fa5ff1865742
zhangyulu123 <zhangyulu123@yahoo.com>
2015-07-01 14:05:33 +0800

    add microblog project file

---
+++ microblog/db_repository/versions/003_migration.py
@@ -0,0 +1,37 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+posts = Table('posts', pre_meta,
+    Column('id', INTEGER, primary_key=True, nullable=False),
+    Column('body', VARCHAR(length=140)),
+    Column('timestamp', DATETIME),
+    Column('user_id', INTEGER),
+)
+
+post = Table('post', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('body', String(length=140)),
+    Column('timestamp', DateTime),
+    Column('user_id', Integer),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['posts'].drop()
+    post_meta.tables['post'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['posts'].create()
+    post_meta.tables['post'].drop()
